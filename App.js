89ae56d6670d2138639e54c28cd8af8d2f827d87
vikasmaliya004 <vikasmaliya004@gmail.com>
2022-06-15 19:10:43 +0530

    new change

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
+let direction = { x: 0, y: 0 };
+let foodSound = new Audio('food.mp3');
+let gameOverSound = new Audio('gameover.mp3');
+let moveSound = new Audio('move.mp3');
+let musicSound = new Audio('music.mp3');
 
+
+
+function main(ctime) {
+    window.requestAnimationFrame(main);
+
+}
